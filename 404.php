bee72a13d165852c5873d0aeccfe90ba4f8e537a
Daryl <Darylgatt40@gmail.com>
2023-11-21 13:47:27 +0100

    new file:   404.php 	modified:   comingsoon.php 	modified:   ourmission.php 	new file:   twig/404.twig 	new file:   twig/comingsoon.twig 	new file:   twig/ourmission.twig 	new file:   vendor/autoload.php 	new file:   vendor/composer/ClassLoader.php 	new file:   vendor/composer/InstalledVersions.php 	new file:   vendor/composer/LICENSE 	new file:   vendor/composer/autoload_classmap.php 	new file:   vendor/composer/autoload_files.php 	new file:   vendor/composer/autoload_namespaces.php 	new file:   vendor/composer/autoload_psr4.php 	new file:   vendor/composer/autoload_real.php 	new file:   vendor/composer/autoload_static.php 	new file:   vendor/composer/installed.json 	new file:   vendor/composer/installed.php 	new file:   vendor/composer/platform_check.php 	new file:   vendor/symfony/polyfill-ctype 	new file:   vendor/symfony/polyfill-mbstring 	new file:   vendor/twig/twig

--- 404.php
+++ 404.php
@@ -1,9 +1,6 @@
 <?php
     require_once 'vendor/autoload.php';
 
-    // Include PHP header
-    require(__DIR__ . '/includes/header.php'); 
-
     // Initialize the Twig FilesystemLoader with the path
     $loader = new \Twig\Loader\FilesystemLoader(__DIR__."/twig");
 
@@ -11,14 +8,11 @@
     $twig = new \Twig\Environment($loader);
 
     // Define the template file you want to render
-    $templateFile = 'comingsoon.twig'; 
+    $templateFile = '404.twig'; 
 
     // Load the template
     $template = $twig->load($templateFile);
 
     // Render the template
-    echo $template->render([]);
-
-    // Include PHP footer
-    require(__DIR__ . '/includes/footer.html'); 
+    echo $template->render([]); //No variable input needed
 ?>
